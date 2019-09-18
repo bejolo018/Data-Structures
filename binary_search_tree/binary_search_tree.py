c08507d805fbdaff3bfc5195362be4ffd2734229
bejolo018 <bejolo018@gmail.com>
2019-09-18 16:43:38 -0500

    knocked out two of the binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -10,11 +10,44 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    current_tree = self
+
+    checking = True
+
+    while checking is True: 
+     
+      if value >= current_tree.value and current_tree.right:
+        current_tree = current_tree.right
+    
+      elif value < current_tree.value and current_tree.left:
+        current_tree = current_tree.left
+     
+      elif value >= current_tree.value and not current_tree.right:
+        current_tree.right = BinarySearchTree(value)
+        checking = False
+      
+      elif value < current_tree.value and not current_tree.left:
+        current_tree.left = BinarySearchTree(value)
+        checking = False
 
   def contains(self, target):
-    pass
+    current_tree = self
 
+    checking = True
+
+    while checking is True:
+      if current_tree.value == target:
+        checking = False
+        return True
+      elif target >= current_tree.value and current_tree.right:
+        current_tree = current_tree.right
+      elif target < current_tree.value and current_tree.left:
+        current_tree = current_tree.left
+      elif target >= current_tree.value and not current_tree.right:
+        return False
+      elif target < current_tree.value and not current_tree.left:
+        return False
+        
   def get_max(self):
     pass
 
