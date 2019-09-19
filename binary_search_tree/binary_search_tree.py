8d156921eb76af89ca3943e4c2f14a90e474fa05
bejolo018 <bejolo018@gmail.com>
2019-09-19 10:49:12 -0500

    couldnt figure out the for each, just pushin what i got

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -49,7 +49,10 @@ class BinarySearchTree:
         return False
         
   def get_max(self):
-    pass
+      if self.right:
+        return self.right.get_max()
+      else:
+        return self.value
 
   def for_each(self, cb):
     pass
